dfb937f7cdcde26fbdd26e8edd45f8ab3afbd388
Szymon Sidor <szsidor@gmail.com>
2024-11-20 10:00:28 +0100

    duuuuze sikibidi sigma zmiany

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from fastapi import FastAPI
-import uvicorn
-
-
+from fastapi import FastAPI, Query
 app = FastAPI()
 
-@app.get('/test1{id}')
-async def test1(id: int):
-    return f'otrzymalem parametr{id}'
+class Game:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-@app.post('/test2{id}')
-async def test2(id: int):
-    return f"dodalem do bazy{id}"
+    def modify_price(self, price):
+        self.price = price
 
-uvicorn.run(app, host='0.0.0.0', port=8000)
+class Store:
+    def __init__(self, store_name):
+        self.store_name = store_name
+        self.games = {}
+        self.game_counter = 0
+
+    def add_game(self, game):
+        self.games[self.game_counter] = game
+        self.game_counter += 1
+        return self.game_counter - 1
+
+    def remove_game(self, id):
+        del self.games[id]
+
+store = Store("SUPER_SKLEP")
+
+@app.post("/add_game/")
+async def hello_world(name: str, price: float):
+    game = Game(name, price)
+    game_id = store.add_game(game)
+    return f"Game {name} added as id: {game_id}"
+
+@app.get("/display_games/")
+async def hello_world():
+    return store.games
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("example:app", reload=True)port=8000)
