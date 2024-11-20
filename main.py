479d2f37cfcd7211c16b767cfe357e6d4ae43fc9
Szymon Sidor <szsidor@gmail.com>
2024-11-20 09:39:54 +0100

    sigma

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get('/test1')
-async def test1():
-    return {'message':'Hello World'}
+@app.get('/test1{id}')
+async def test1(id: int):
+    return f'otrzymalem parametr{id}'
+
+@app.post('/test2{id}')
+async def test2(id: int):
+    return f"dodalem do bazy{id}"
 
 uvicorn.run(app, host='0.0.0.0', port=8000)
